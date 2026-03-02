fa0ba79187f55247cb381d3be314c994ae2585e7
Mariana Sonego <sonego.mariana@aluno.ifsp.edu.br>
2026-03-02 14:33:38 -0300

    feat: adding wecomp 2025 edition

--- src/assets/content/editions.ts
+++ src/assets/content/editions.ts
@@ -3,10 +3,17 @@ import wecompII from "../images/editionsLogo/wecomp2018.svg";
 import wecompIII from "../images/editionsLogo/wecomp2019.svg";
 import wecompIV from "../images/editionsLogo/wecomp2023.svg";
 import wecompV from "../images/editionsLogo/wecomp2024.svg";
+import wecompVI from "../images/editionsLogo/wecomp2025.svg";
 
 import { EditionProps } from "../../components/sections/Editions/types";
 
 export const editionsContent: EditionProps[] = [
+  {
+    logoPath: wecompVI,
+    altText: "Logo da VI Wecomp",
+    edition: "Wecomp 2025",
+    website: "https://wecomp-vi.vercel.app",
+  },
   {
     logoPath: wecompV,
     altText: "Logo da V Wecomp",
