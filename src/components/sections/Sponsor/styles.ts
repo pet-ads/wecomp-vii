490aab2f3ddba4cc0ad3f923871470fcac65c524
Mariana Sonego <sonegomariana22@gmail.com>
2026-04-15 10:19:20 -0300

    fix: fix SoonCard layout position

--- src/components/sections/Sponsor/styles.ts
+++ src/components/sections/Sponsor/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  padding: 40px 0; /* Adiciona um respiro em cima e embaixo */
 
   display: flex;
   align-items: center;
